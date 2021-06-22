5860a42b060d86eea7fdaaef1aa40082c90a73a0
Dominik Bucher <d.bucher@centrum.cz>
2021-06-22 13:29:04 +0200

    Generate kebab-cased links to other files

--- Sources/swift-doc/Supporting Types/Components/Relationships.swift
+++ Sources/swift-doc/Supporting Types/Components/Relationships.swift
@@ -88,7 +88,7 @@ struct Relationships: Component {
                         } else {
                             let absoluteTypePath = URL(fileURLWithPath: String(type.filePath.dropFirst("file://".count))).deletingLastPathComponent()
                             let relativePathToInheritedClass = absoluteTypePath.relativePath(from: URL(fileURLWithPath: symbol.filePath).deletingLastPathComponent())!
-                    return "[`\(type.id)`](\(path(for: type, with: relativePathToInheritedClass)))"
+                    return "[`\(type.id)`](\(path(for: type, with: relativePathToInheritedClass).camelCaseToKebabCase()))"
                         }
                     }.joined(separator: ", "))
                     """#
@@ -138,6 +138,22 @@ struct Relationships: Component {
         """#
     }
 }
+
+extension String {
+    func camelCaseToKebabCase() -> String {
+        let acronymPattern = "([A-Z]+)([A-Z][a-z]|[0-9])"
+        let normalPattern = "([a-z0-9])([A-Z])"
+        return self.processCamalCaseRegex(pattern: acronymPattern)?
+            .processCamalCaseRegex(pattern: normalPattern)?.lowercased() ?? self.lowercased()
+    }
+    
+    fileprivate func processCamalCaseRegex(pattern: String) -> String? {
+        let regex = try? NSRegularExpression(pattern: pattern, options: [])
+        let range = NSRange(location: 0, length: count)
+        return regex?.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "$1-$2")
+    }
+}
+
 
 extension URL {
     func relativePath(from base: URL) -> String? {
